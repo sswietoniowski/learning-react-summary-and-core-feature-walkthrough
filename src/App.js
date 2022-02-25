6aa0de66983356c43276c249f4212599ed448070
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-25 16:30:14 +0100

    Created components: Todo, Modal and Backdrop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Todo from "./components/Todo";
+import Modal from "./components/Modal";
+import Backdrop from "./components/Backdrop";
+
 function App() {
-  return <div>Hello, world!</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo title="Roll" />
+      <Todo title="Eat" />
+      <Todo title="Sleep" />
+      <span>Repeat :-)</span>
+      <Modal />
+      <Backdrop />
+    </div>
+  );
 }
 
 export default App;
