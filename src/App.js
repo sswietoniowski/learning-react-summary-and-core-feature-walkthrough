7702d38b4c5405e50911f32ff6e7f396eb83a2d0
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-26 17:37:36 +0100

    Added routing and navigation links

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" exact element={<AllMeetupsPage />} />
-      <Route path="/new-meetup" element={<NewMeetupPage />} />
-      <Route path="/favorites" element={<FavoritesPage />} />
-    </Routes>
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" exact element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
   );
 }
 
