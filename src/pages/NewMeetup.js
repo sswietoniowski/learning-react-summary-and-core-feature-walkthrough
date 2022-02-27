091e7932c945f5bbfec983174f737d374b904a02
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-27 10:33:36 +0100

    Added navigation programmatically

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,15 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+
   function addMeetupHandler(meetupData) {
     const apiUrl =
       "https://react-getting-started-1c270-default-rtdb.europe-west1.firebasedatabase.app/"; //process.env.REACT_APP_FIREBASE_DATABASE_URL;
+
     fetch(`${apiUrl}meetups.json`, {
       method: "POST",
       body: JSON.stringify(meetupData),
       headers: {
         "Content-Type": "application/json",
       },
+    }).then(() => {
+      navigate("/");
     });
   }
 
