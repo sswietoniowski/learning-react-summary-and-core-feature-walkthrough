423263d720681de135bb71edc680e03449a1117b
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-27 10:27:32 +0100

    Configured FireBase backend, send post

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,22 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    const apiUrl =
+      "https://react-getting-started-1c270-default-rtdb.europe-west1.firebasedatabase.app/"; //process.env.REACT_APP_FIREBASE_DATABASE_URL;
+    fetch(`${apiUrl}meetups.json`, {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
